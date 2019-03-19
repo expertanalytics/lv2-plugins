4917a4f98516176fe190c405d38694309ed12d3e
Vinzenz Gregor Eck <vinzenz@xal.no>
2019-03-19 11:50:44 +0100

    Added scaffold for our plugin

--- simple_delay/simple_delay.c
+++ simple_delay/simple_delay.c
@@ -2,3 +2,90 @@
 // Created by vinzenz on 3/19/19.
 //
 
+#include <math.h>
+#include <stdlib.h>
+
+#include "lv2/lv2plug.in/ns/lv2core/lv2.h"
+#define SIMPLE_DELAY_URI "https://github.com/expertanalytics/lv2-plugins/tree/master/simple_delay"
+
+
+typedef enum {
+    AMP_INPUT  = 0,
+    AMP_OUTPUT = 1
+} PortIndex;
+
+
+typedef struct {
+    // Port buffers
+    const float* input;
+    float*       output;
+} SimpleDelay;
+
+
+static LV2_Handle
+instantiate(const LV2_Descriptor*     descriptor,
+            double                    rate,
+            const char*               bundle_path,
+            const LV2_Feature* const* features)
+{
+    SimpleDelay* simple_delay = (SimpleDelay*)calloc(1, sizeof(SimpleDelay));
+
+    return (LV2_Handle)simple_delay;
+}
+
+
+static void
+connect_port(LV2_Handle instance,
+             uint32_t   port,
+             void*      data)
+{
+}
+
+static void
+activate(LV2_Handle instance)
+{
+}
+
+static void
+run(LV2_Handle instance, uint32_t n_samples)
+{
+}
+
+static void
+deactivate(LV2_Handle instance)
+{
+}
+
+static void
+cleanup(LV2_Handle instance)
+{
+    free(instance);
+}
+
+static const void*
+extension_data(const char* uri)
+{
+    return NULL;
+}
+
+
+static const LV2_Descriptor descriptor = {
+        SIMPLE_DELAY_URI,
+        instantiate,
+        connect_port,
+        activate,
+        run,
+        deactivate,
+        cleanup,
+        extension_data
+};
+
+LV2_SYMBOL_EXPORT
+const LV2_Descriptor*
+lv2_descriptor(uint32_t index)
+{
+switch (index) {
+case 0:  return &descriptor;
+default: return NULL;
+}
+}
