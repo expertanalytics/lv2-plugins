ea8c72e3467b39dea54813c52380d0a3e045074d
Trygve Bærland <trygve.baerland@gmail.com>
2019-10-01 16:11:12 +0200

    Removed waflib stuff.

---
+++ simple_delay/cmake-build-debug/CMakeFiles/simple_delay.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_delay.dir/simple_delay.o"
+  "libsimple_delay.dylib"
+  "libsimple_delay.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/simple_delay.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
