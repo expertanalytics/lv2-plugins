faadfb4d70b318388fb08ad05094cd97d09e6ad5
guttorm <guttorm.kvaal@gmail.com>
2019-11-26 12:15:18 +0100

    script for generating project template files and folder

--- plugin_template/template.c
+++ plugin_template/template.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 
 #include "lv2/lv2plug.in/ns/lv2core/lv2.h"
-#define q "https://github.com/expertanalytics/lv2-plugins/tree/master/foo"
+#define q "https://github.com/expertanalytics/lv2-plugins/tree/master/plugin_name_placeholder"
 
 
 typedef enum {
@@ -20,9 +20,12 @@ typedef struct {
     // Port buffers
     const float* input;
     float*       output;
-} Foo;
+} TypeNamePlaceholder;
 
 
+/*
+ * Instanciates a plugin.
+ */
 static LV2_Handle
 instantiate(
     const LV2_Descriptor* descriptor,
@@ -32,43 +35,61 @@ instantiate(
     const* features,
 )
 {
-    Foo* foo = (Foo*)malloc(sizeof(foo));
+    TypeNamePlaceholder* plugin_name_placeholder = (TypeNamePlaceholder*)malloc(sizeof(plugin_name_placeholder));
 
-    return (LV2_Handle)foo;
+    return (LV2_Handle)plugin_name_placeholder;
 }
 
-
+/*
+ * connect_port is called by the host to connect a port to a buffer.
+ */
 static void
 connect_port(LV2_Handle instance,
              uint32_t   port,
              void*      data)
 {
-    Foo* foo = (Foo*)instance;
+    TypeNamePlaceholder* plugin_name_placeholder = (TypeNamePlaceholder*)instance;
 
     switch ((PortIndex)port) {
         case INPUT_PORT:
-            foo->input = (const float*)data;
+            plugin_name_placeholder->input = (const float*)data;
             break;
         case OUTPUT_PORT:
-            foo->output = (float*)data;
+            plugin_name_placeholder->output = (float*)data;
             break;
     }
 }
 
+/*
+ * Activates a plugin instance. Is called before run().
+ */
 static void
 activate(LV2_Handle instance) {}
 
+/*
+ * Run a plugin instance for a block
+ */
 static void
 run(LV2_Handle instance, uint32_t n_samples) {}
 
+/*
+ * Deactivates a plugin instance. Is called after run().
+ */
 static void
 deactivate(LV2_Handle instance) {}
 
+/*
+ * Cleanes up a plugin instance. Is the counterpart to instanciate.
+ */
 static void
 cleanup(LV2_Handle instance){
     free(instance);
 }
 
+/*
+ * Returns additional plugin data. Typically used to return a struct with functions pointers
+ * to extend the LV2_Descriptor API.
+ */
 static const void*
 extension_data(const char* uri)
 {
@@ -89,6 +110,11 @@ static const LV2_Descriptor descriptor = {
 
 LV2_SYMBOL_EXPORT
 const LV2_Descriptor*
+
+/*
+ * A plugin accessor. Plugings are accessed by index using values from 0 and up.
+ * Out of range indices must return NULL.
+ */
 lv2_descriptor(uint32_t index)
 {
 switch (index) {
