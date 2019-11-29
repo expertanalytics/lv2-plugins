1e107426501141715a554e1f3f2098d6ebe874ac
Trygve Bærland <trygve.baerland@gmail.com>
2019-11-29 09:40:31 +0100

    Basic idea for manifest file template expansion in place, but something in cmake is not working.

--- build_support/add_plugin.cmake
+++ build_support/add_plugin.cmake
@@ -1,26 +1,37 @@
 # Utiltity functions for building and installing LV2 plugins.
 
-function(add_plugin plugin_name version)
+function(add_plugin plugin_name)
+        set(bundle ${plugin_name}.lv2)
+        file(MAKE_DIRECTORY ${bundle})
         set(source ${plugin_name}/${plugin_name}.c)
-        add_library(${plugin_name} SHARED ${source})
-        target_link_libraries(${plugin_name} PUBLIC ${LV2_LIBRARIES})
-        set_target_properties(${plugin_name} PROPERTIES PREFIX ""
-                        VERSION ${version})
+        set(target ${plugin_name})
+        add_library(${target} SHARED ${source})
+        target_link_libraries(${target} PUBLIC ${LV2_LIBRARIES})
+        set_target_properties(${target} PROPERTIES 
+                        PREFIX ""
+                        SUFFIC ".${LIB_EXT}"
+                        VERSION "1.0")
 
         # And install:
-        set(root ${LV2_INSTALL_DIR}/${plugin_name}.lv2/)
-        install(TARGETS ${plugin_name}
-                DESTINATION ${root})
-        # Then we install the turtle files:
+        set(root ${LV2_INSTALL_DIR})
+        # Then we install the turtle 
         # TODO: The main turtle has to insert version number.
-        install(CODE "")
-        execute_process(COMMAND ....
-                        INPUT_FILE plugin_template/manifest.ttl.in
-                        OUTPUT_FILE ${root}/manifest.ttl)
+        # Make manifest file:
+        execute_process(COMMAND bash "-c" 
+                       "cat ${PROJECT_SOURCE_DIR}/plugin_template/manifest.ttl.in | 
+                        sed 's/@PLUGIN_NAME@/${plugin_name}/g;s/\@LIB_EXT\@/${LIB_EXT}/g'"
+                        OUTPUT_FILE ${bundle}/manifest.ttl)
+        install(DIRECTORY ${bundle} 
+                DESTINATION ${root})
+#install(CODE "execute_process(COMMAND deploy_turtle.sh -m ${plugin_name} dylib
+#               OUTPUT_FILE ${root}/manifest.ttl)")
+#execute_process(COMMAND ....
+#                        INPUT_FILE plugin_template/manifest.ttl.in
+#                        OUTPUT_FILE ${root}/manifest.ttl)
         # TODO: The manifest file should add plugin_name appropriately and LIB_EXT suffix
-        execute_process(COMMAND ....
-                        INPUT_FILE ${plugin_name}/${plugin_name}.ttl
-                        OUTPUT_FILE ${root}/${plugin_name}.ttl)
+        #execute_process(COMMAND ....
+#                INPUT_FILE ${plugin_name}/${plugin_name}.ttl
+#                        OUTPUT_FILE ${root}/${plugin_name}.ttl)
         # TODO: Process manifest.ttl.in file (basically a template)
         # TODO: Process .ttl file (insert version)
 
--- build_support/deploy_turtle_files.sh
+++ build_support/deploy_turtle_files.sh
@@ -2,7 +2,9 @@
 # Function for expanding manifest.ttl.in based on template in
 # plugin_template/manifest.ttl.in
 expand_manifest() {
-        sed -e 's/@PLUGIN_NAME@/'$1'/g' plugin_template/manifest.ttl.in | sed -e 's/@LIB_EXT@/'$2'/g'
+        echo "INSIDE expand_manifest\n"
+        sed -e 's/@PLUGIN_NAME@/'$1'/g' -e 's/@LIB_EXT@/'$2'/g' plugin_template/manifest.ttl.in
+        echo "LEAVING expand_manifest\n"
 }
 
 case "$1" in
