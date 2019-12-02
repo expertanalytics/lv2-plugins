b89c9e5f10dc4943a3c932cba3ac45fc57666099
Trygve Bærland <trygve.baerland@gmail.com>
2019-12-02 14:11:27 +0100

    A new plugin can now be made and installed without error. Still need to fix version file stuff

--- new_plugin.sh
+++ new_plugin.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 
 # Script for generating lv2 templates and plugin project folder.
 # The name of the project is taken as input.
 
 plugin_name_input=$1
-echo ${plugin_name_input}
 snake_var=${plugin_name_input,,}
 pascal_var=""
 
@@ -14,13 +13,19 @@ for i in "${ADDR[@]}"; do
 done
 
 base_folder=${snake_var}
+[ ! -d "${snake_var}" ] && mkdir -p "${base_folder}"
 
-cp -r plugin_template ${base_folder}
+cp plugin_template/{template.ttl,template.c,version.txt}  ${base_folder}
 mv ${base_folder}/template.ttl ${base_folder}/${snake_var}.ttl
 mv ${base_folder}/template.c ${base_folder}/${snake_var}.c
 
-sed -i 's/\<TypeNamePlaceholder\>/'${pascal_var}'/g' ${base_folder}/${snake_var}.c
-sed -i 's/\<TypeNamePlaceholder\>/'${pascal_var}'/g' ${base_folder}/${snake_var}.ttl
-sed -i 's/\<plugin_name_placeholder\>/'${snake_var}'/g' ${base_folder}/${snake_var}.c
-sed -i 's/\<plugin_name_placeholder\>/'${snake_var}'/g' ${base_folder}/${snake_var}.ttl
-#sed -i 's/\<plugin_name_placeholder\>/'${snake_var}'/g' ${base_folder}/manifest.ttl.in
+sed -i '' 's/\<TypeNamePlaceholder\>/'${pascal_var}'/g' ${base_folder}/${snake_var}.c
+sed -i '' 's/\<TypeNamePlaceholder\>/'${pascal_var}'/g' ${base_folder}/${snake_var}.ttl
+sed -i '' 's/plugin_name_placeholder/'${snake_var}'/g' ${base_folder}/${snake_var}.c
+sed -i '' 's/plugin_name_placeholder/'${snake_var}'/g' ${base_folder}/${snake_var}.ttl
+sed -i '' 's/PLUGIN_NAME_PLACEHOLDER/'${snake_var^^}'/g' ${base_folder}/${snake_var}.c
+
+# Add line to CMakeLists.txt
+sed -i '' '/^\# END of list of plugins/i\
+            add_plugin\('${snake_var}'\)
+' CMakeLists.txt
