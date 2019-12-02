94e0af7c4f440415597acd02e5a02968cf674eb2
Trygve Bærland <trygve.baerland@gmail.com>
2019-12-02 10:18:14 +0100

    Finally able create and install manifest file reliably.

--- build_support/add_plugin.cmake
+++ build_support/add_plugin.cmake
@@ -2,27 +2,37 @@
 
 function(add_plugin plugin_name)
         set(bundle ${plugin_name}.lv2)
-        file(MAKE_DIRECTORY ${bundle})
+
+        get_filename_component(build_bundle ${bundle}
+                        REALPATH
+                        BASE_DIR ${CMAKE_BINARY_DIR})
+
+        file(MAKE_DIRECTORY ${build_bundle})
+
         set(source ${plugin_name}/${plugin_name}.c)
         set(target ${plugin_name})
+
         add_library(${target} SHARED ${source})
         target_link_libraries(${target} PUBLIC ${LV2_LIBRARIES})
         set_target_properties(${target} PROPERTIES 
                         PREFIX ""
                         SUFFIC ".${LIB_EXT}"
-                        VERSION "1.0")
+                        VERSION "1.0"
+                        LIBRARY_OUTPUT_DIRECTORY ${build_bundle})
         # And install:
         set(root ${LV2_INSTALL_DIR})
         # Then we install the turtle 
-        # TODO: The main turtle has to insert version number.
         # Make manifest file:
         execute_process(COMMAND bash "-c" 
                        "cat ${PROJECT_SOURCE_DIR}/plugin_template/manifest.ttl.in | 
                         sed 's/@PLUGIN_NAME@/${plugin_name}/g;s/\@LIB_EXT\@/${LIB_EXT}/g'"
-                        OUTPUT_FILE ${bundle}/manifest.ttl)
+                        OUTPUT_FILE ${build_bundle}/manifest.ttl)
+        # TODO: Make main turtle file:
+
+        # Install files:
         install(DIRECTORY ${bundle} 
                 DESTINATION ${root})
-        install(FILES ${CMAKE_BINARY_DIR}/${bundle}/manifest.ttl
+        install(FILES ${build_bundle}/manifest.ttl
                 DESTINATION ${root}/${bundle})
         install(TARGETS ${target}
                 DESTINATION ${root}/${bundle})
